3917207c367e7275902a9749b9f765ffe7fffc33
krish12345y89 <166594630+krish12345y89@users.noreply.github.com>
2024-12-01 13:23:59 +0530

    Update form.php

--- form.php
+++ form.php
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admission Form</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style/style.css">
     <script>
         function validateForm() {
             const requiredFields = ['name', 'email', 'phone', 'father_name', 'mother_name', 'dmc_10th', 'dmc_12th', 'branch', 'semester'];
